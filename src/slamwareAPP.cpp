e5105ec05e0d9dfb60e72478481f07865a426830
kint.zhao <kint.zhao@slamtec.com>
2017-07-22 22:30:31 +0800

    add subsciber goal and vel and publisher plan_path, modify tf frame

--- src/slamwareAPP.cpp
+++ src/slamwareAPP.cpp
@@ -5,13 +5,13 @@
 #include  "slamwareAPP.h"
 
 slamwareAPP::slamwareAPP(std::string ip):workOk(false),ip_addres_(ip),
-    nh_("~"), robot_pose_pub_thread_(NULL),scan_pub_thread_(NULL),map_pub_thread_(NULL)
+    nh_("~"), robot_pose_pub_thread_(NULL),scan_pub_thread_(NULL),map_pub_thread_(NULL),plan_path_pub_thread_(NULL)
 {
 
 }
 
 slamwareAPP::slamwareAPP():workOk(false), ip_addres_("192.168.11.1"),
-    nh_("~"), robot_pose_pub_thread_(NULL),scan_pub_thread_(NULL),map_pub_thread_(NULL)
+    nh_("~"), robot_pose_pub_thread_(NULL),scan_pub_thread_(NULL),map_pub_thread_(NULL),plan_path_pub_thread_(NULL)
 {
 
 }
@@ -28,6 +28,15 @@ slamwareAPP::~slamwareAPP()
 
     if(map_pub_thread_){
         delete map_pub_thread_;
+    }
+
+    if(transform_thread_)
+    {
+        delete transform_thread_;
+    }
+    if(plan_path_pub_thread_)
+    {
+        delete plan_path_pub_thread_;
     }
 }
 
@@ -50,23 +59,32 @@ bool slamwareAPP::connectSlamware()
 
 void slamwareAPP::init()
 {
-    robot_frame_ = "/odom";
+    robot_frame_ = "/base_link";
+    odom_frame_ = "/odom";
     laser_frame_ = "/laser";
     map_frame_ = "/map";
 
-    robot_pose_pub_period_ = 0.01;
+    topic_control = "/cmd_vel";
+    topic_goal = "/move_base_simple/goal";
+
+    robot_pose_pub_period_ = 0.05;
     scan_pub_period_ = 0.1;
     map_pub_period_ = 0.2;
+    transform_publish_period = 0.05;
 
     scan_pub_ = nh_.advertise<sensor_msgs::LaserScan>("scan", 10);
     robot_pose_pub_ = nh_.advertise<nav_msgs::Odometry> ("odom", 10);
     gridmap_pub_ = nh_.advertise<nav_msgs::OccupancyGrid> ("map", 1, true);
     gridmap_info_pub_ = nh_.advertise<nav_msgs::MapMetaData>("map_metadata", 1, true);
-    //robot_vel_sub_ = nh_.subscribe ("/cmd_vel", 10, robotVelocityCallback);
+    global_plan_pub_ = nh_.advertise<nav_msgs::Path>("global_plan_path", 2);
+
+    robot_vel_sub_ = nh_.subscribe (topic_control, 10, &slamwareAPP::robotControlCallback, this);
+    goal_sub_ = nh_.subscribe (topic_goal, 10, &slamwareAPP::moveToGoalCallback, this);
 
     robot_pose_pub_thread_ = new boost::thread(boost::bind(&slamwareAPP::publishRobotPose, this, robot_pose_pub_period_));
     scan_pub_thread_ = new boost::thread(boost::bind(&slamwareAPP::publishScan, this, scan_pub_period_));
     map_pub_thread_ = new boost::thread(boost::bind(&slamwareAPP::publishMap, this, map_pub_period_));
+    plan_path_pub_thread_ = new boost::thread(boost::bind(&slamwareAPP::publishPlanPath, this, transform_publish_period));
 
 }
 
@@ -75,38 +93,48 @@ void slamwareAPP::startSlamwareWork()
     init();
 }
 
-void slamwareAPP::publishRobotPose(double transform_publish_period)
+void slamwareAPP::publishRobotPose(double publish_period)
 {
-    if(transform_publish_period == 0)
+    if(publish_period == 0)
         return;
 
-    ros::Rate r(1.0 / transform_publish_period);
+    ros::Rate r(1.0 / publish_period);
     while(ros::ok())
     {
-        rpos::core::Location location = SDP_.getLocation();
-        std::cout << "Robot Location: " << std::endl;
-        std::cout << "x: " << location.x() << ", ";
-        std::cout << "y: " << location.y() << std::endl;
+        //send TF transform
+        tf::Transform identy_transform;
+        identy_transform.setOrigin (tf::Vector3 (0.0, 0.0, 0.0));
+        identy_transform.setRotation (tf::Quaternion(0,0,0,1));
+        tfB_.sendTransform ( tf::StampedTransform (identy_transform, ros::Time::now (), map_frame_,
+                                      odom_frame_));
+        tfB_.sendTransform ( tf::StampedTransform (identy_transform, ros::Time::now (), robot_frame_,
+                                      laser_frame_));
 
-        rpos::core::Pose pose = SDP_.getPose();
-        std::cout << "Robot Pose: " << std::endl;
-        std::cout << "x: " << pose.x() << ", ";
-        std::cout << "y: " << pose.y() << ", ";
-        std::cout << "yaw: " << pose.yaw() << std::endl;
-
+        //check power
         int battPercentage = SDP_.getBatteryPercentage();
-        std::cout <<"Battery: " << battPercentage << "%" << std::endl;
+        if(battPercentage < 10)
+            std::cout <<"lower power!! Battery: " << battPercentage << "%" << std::endl;
 
+        rpos::core::Location location = SDP_.getLocation();
+        rpos::core::Pose pose = SDP_.getPose();
 
+        //publish odom transform
+        tf::Transform transform;
+        transform.setOrigin (tf::Vector3 (pose.x(), pose.y(), 0.0));
+        tf::Quaternion q = tf::createQuaternionFromYaw (pose.yaw());
+        transform.setRotation (q);
+        tfB_.sendTransform ( tf::StampedTransform (transform, ros::Time::now (), odom_frame_,
+                                      robot_frame_));
+
+        //send TF transform
         nav_msgs::Odometry robotPose;
-        robotPose.header.frame_id = robot_frame_;
+        robotPose.header.frame_id = map_frame_;
         robotPose.header.stamp = ros::Time::now ();
         robotPose.pose.pose.position.x = pose.x();
         robotPose.pose.pose.position.y = pose.y();
-        robotPose.pose.pose.orientation = tf::createQuaternionMsgFromYaw (
-                    pose.yaw());
-
+        robotPose.pose.pose.orientation = tf::createQuaternionMsgFromYaw(pose.yaw());
         robot_pose_pub_.publish (robotPose);
+                r.sleep();
     }
 }
 
@@ -121,7 +149,6 @@ void slamwareAPP::publishScan(double transform_publish_period)
         ros::Time start_scan_time = ros::Time::now();
         rpos::features::system_resource::LaserScan ls = SDP_.getLaserScan();
         ros::Time end_scan_time = ros::Time::now();
-
         double scan_time = (end_scan_time - start_scan_time).toSec() * 1e-3;
 
         std::vector<rpos::core::LaserPoint> points = ls.getLaserPoints();
@@ -133,9 +160,8 @@ void slamwareAPP::publishScan(double transform_publish_period)
 
         scan_msg.header.stamp = start_scan_time;
         scan_msg.header.frame_id = laser_frame_;
-        scan_msg.angle_min =  points.begin()->angle();
-        scan_msg.angle_max =  points.end()->angle();
-
+        scan_msg.angle_min =  points.front().angle();
+        scan_msg.angle_max =  points.back().angle();
         scan_msg.angle_increment = (scan_msg.angle_max - scan_msg.angle_min) / (double)(points.size()-1);
 
         scan_msg.scan_time = scan_time;
@@ -154,19 +180,18 @@ void slamwareAPP::publishScan(double transform_publish_period)
                 continue;
             }
             scan_msg.ranges[i] = points[i].distance();
-            std::cout << "distance: " << points[i].distance() << ", angle: " << points[i].angle() << ", valid: " << (points[i].valid()?"true":"false") << std::endl;
         }
         scan_pub_.publish(scan_msg);
+        r.sleep();
     }
-
 }
 
-void slamwareAPP::publishMap(double transform_publish_period)
+void slamwareAPP::publishMap(double publish_period)
 {
-    if(transform_publish_period == 0)
+    if(publish_period == 0)
         return;
 
-    ros::Rate r(1.0 / transform_publish_period);
+    ros::Rate r(1.0 / publish_period);
     while(ros::ok())
     {
         Map map = SDP_.getMap(MapTypeBitmap8Bit, rpos::core::RectangleF(-10, -10, 20, 20), rpos::features::location_provider::EXPLORERMAP);
@@ -212,8 +237,70 @@ void slamwareAPP::publishMap(double transform_publish_period)
     }
 }
 
-//void robotVelocityCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
-//{
-//    std::cout<<"subscribe vel "<<std::endl;
-//}
+void slamwareAPP::publishPlanPath(float publish_period)
+{
+    if(publish_period == 0)
+        return;
+
+    ros::Rate r(1.0 / publish_period);
+    while(ros::ok())
+    {
+        rpos::actions::MoveAction moveAction = SDP_.getCurrentAction();
+
+        if(moveAction)
+        {
+            rpos::features::motion_planner::Path path = moveAction.getRemainingPath();
+            std::vector<rpos::core::Location> locations = path.getPoints();
+
+            //create a path message
+            nav_msgs::Path paths;
+            paths.poses.resize(locations.size());
+            paths.header.frame_id = map_frame_;
+            paths.header.stamp = ros::Time();
+
+            for(int i = 0; i< locations.size(); i++)
+            {
+                geometry_msgs::PoseStamped pose_stamp;
+                pose_stamp.header.frame_id = map_frame_;
+                pose_stamp.header.stamp = ros::Time();
+                pose_stamp.pose.position.x = locations[i].x();
+                pose_stamp.pose.position.y = locations[i].y();
+                pose_stamp.pose.position.z = 0.0;
+                pose_stamp.pose.orientation.w = 1;
+                pose_stamp.pose.orientation.x = 0;
+                pose_stamp.pose.orientation.y = 0;
+                pose_stamp.pose.orientation.z = 0;
+                paths.poses[i] = pose_stamp;
+            }
+            global_plan_pub_.publish(paths);
+        }
+        r.sleep();
+    }
+}
+
+void slamwareAPP::robotControlCallback (const geometry_msgs::TwistConstPtr &vel)
+{
+    //std::cout<<" "<<vel->linear.x <<"  "<<vel->angular.z<<std::endl;
+    if(vel->linear.x > 0.005f)
+        rpos::actions::MoveAction act = SDP_.moveBy(ACTION_DIRECTION::FORWARD);
+    else   if(vel->linear.x < -0.005f)
+        rpos::actions::MoveAction act = SDP_.moveBy(ACTION_DIRECTION::BACKWARD);
+
+    if( vel->angular.z > 0.001f)
+        rpos::actions::MoveAction act = SDP_.moveBy(ACTION_DIRECTION::TURNLEFT);
+    else if( vel->angular.z < -0.001f)
+        rpos::actions::MoveAction act = SDP_.moveBy(ACTION_DIRECTION::TURNRIGHT);
+
+    return;
+}
+
+void slamwareAPP::moveToGoalCallback (const geometry_msgs::PoseStamped::ConstPtr& goal)
+{
+    //std::cout<<" "<<goal->pose.position.x <<"  "<<goal->pose.position.y;
+    rpos::actions::MoveAction moveAction = SDP_.getCurrentAction();
+    boost::this_thread::sleep_for(boost::chrono::milliseconds(2000));
+    moveAction = SDP_.moveTo(rpos::core::Location(goal->pose.position.x, goal->pose.position.y), false, true);
+
+    return;
+}
 
--- src/slamwareAPP.h
+++ src/slamwareAPP.h
@@ -7,6 +7,8 @@
 #include "std_srvs/Empty.h"
 #include "std_msgs/Float64.h"
 #include "nav_msgs/GetMap.h"
+#include <nav_msgs/Path.h>
+
 
 #include <geometry_msgs/Twist.h>
 #include <nav_msgs/Odometry.h>
@@ -20,11 +22,13 @@
 #include <boost/thread.hpp>
 
 #include <rpos/robot_platforms/slamware_core_platform.h>
+#include <rpos/robot_platforms/slamware_core_platform.h>
 
 using namespace rpos::robot_platforms;
 using namespace rpos::features;
 using namespace rpos::features::location_provider;
 using namespace rpos::system::types;
+using namespace rpos::core;
 
 class slamwareAPP {
 public:
@@ -36,17 +40,21 @@ public:
     bool connectSlamware();
     void startSlamwareWork();
 
-    void robotVelocityCallback(const sensor_msgs::LaserScan::ConstPtr& scan);
-
-    void publishRobotPose(double transform_publish_period);
-    void publishScan(double transform_publish_period);
-    void publishMap(double transform_publish_period);
+    void publishRobotPose(double publish_period);
+    void publishScan(double publish_period);
+    void publishMap(double publish_period);
+    void publishPlanPath(float publish_period);
+    void robotControlCallback (const geometry_msgs::TwistConstPtr &vel);
+    void moveToGoalCallback (const geometry_msgs::PoseStamped::ConstPtr& goal);
 
     ros::Publisher scan_pub_;
     ros::Publisher robot_pose_pub_;
     ros::Publisher gridmap_pub_;
     ros::Publisher gridmap_info_pub_;
+    ros::Publisher global_plan_pub_;
+
     ros::Subscriber robot_vel_sub_;
+    ros::Subscriber goal_sub_;
 
 private:
     ros::NodeHandle nh_;
@@ -57,16 +65,22 @@ private:
     float robot_pose_pub_period_;
     float scan_pub_period_;
     float map_pub_period_;
+    float transform_publish_period;
+
+    tf::TransformBroadcaster tfB_;
 
     boost::thread*  robot_pose_pub_thread_;
     boost::thread* scan_pub_thread_;
     boost::thread* map_pub_thread_;
+    boost::thread* transform_thread_;
+    boost::thread* plan_path_pub_thread_;
 
     std::string robot_frame_;
     std::string laser_frame_;
     std::string map_frame_;
     std::string odom_frame_;
 
-
+    std::string topic_control;
+    std::string topic_goal;
 };
 
